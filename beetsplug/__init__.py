e6f27611c0a96b42f8a65ae8bd32add46992c7f0
d-rk <dirkwilden@gmail.com>
2025-01-20 11:37:06 +0100

    Update __init__.py

--- beetsplug/__init__.py
+++ beetsplug/__init__.py
@@ -1,2 +1,3 @@
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+# -*- coding: utf-8 -*-
+"""Package for the beets-artistart plugin."""
+__import__('pkg_resources').declare_namespace(__name__)
